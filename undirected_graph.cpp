911c3e675d827680b7818f3cf035998a8912341b
ridhamSihag <sihagridham553@gmail.com>
2026-09-01 17:40:57 +0530

    added undirected graph

---
+++ undirected_graph.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Graph {
+    int V;
+    list<int> *l; //initializing a dynamic array.  ->assume we want an input user enter his enput and based on that input we want an array normal array will return error but we can do this by dynamic array.
+public:
+    Graph(int V){
+        this->V = V;
+        l = new list<int> [V]; //Format of dynamic array
+
+    }
+    void addEdge(int u , int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void printAdjList(){ //print neighours of all vertex
+        for(int i = 0 ; i<V ;i++){
+            cout<< i << " : ";
+            for(int neigh : l[i]){
+                cout << neigh << " ";
+            } 
+            cout<< endl;
+        }
+
+    }
+
+};
+int main(){
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+
+    g.printAdjList();
+    return 0;
+
+}
